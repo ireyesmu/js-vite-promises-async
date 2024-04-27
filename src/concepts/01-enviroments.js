d66676e1e933e96ed2ef1de7ae7b4d80678dcc76
Ignacio Reyes <ignacio.reyesmu@gmail.com>
2024-04-27 00:10:26 -0400

    01-enviroments creado

--- src/concepts/01-enviroments.js
+++ src/concepts/01-enviroments.js
@@ -12,6 +12,8 @@ export const enviromentsComponent = ( element ) => {
         const html = `
             Dev: ${ import.meta.env.DEV } <br/>
             Prod: ${ import.meta.env.PROD } <br/>
+            KEY: ${ import.meta.env.VITE_API_KEY } <br/>
+            URL: ${ import.meta.env.VITE_BASE_URL } <br/>
         `;
 
         element.innerHTML = html;
