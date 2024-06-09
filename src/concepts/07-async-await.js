36a9ebb7d38130ff118d26c77890f5b9f0a94941
Ignacio Reyes <ignacio.reyesmu@gmail.com>
2024-06-09 00:54:35 -0400

    Sección 11 finalizada

---
+++ src/concepts/07-async-await.js
@@ -0,0 +1,54 @@
+
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const asyncAwait2Component = async( element ) => {
+
+    console.time('Start');
+
+    // Llama las 3 funciones en forma secuencial, relantizando la ejecución del programa
+    // const value1 = await slowPromise();
+    // const value2 = await mediumPromise();
+    // const value3 = await fastPromise();
+
+    // Optimizando la ejecución simultánea de múltiples promises
+    const [ value1, value2, value3 ] = await Promise.all([
+        slowPromise(),
+        mediumPromise(),
+        fastPromise(),
+    ]);
+
+    element.innerHTML = `
+        value1: ${ value1 } <br/>
+        value2: ${ value2 } <br/>
+        value3: ${ value3 } <br/>
+    
+    `
+    console.timeEnd('Start');
+
+}
+
+
+
+
+
+
+const slowPromise = () => new Promise ( resolve => {
+    setTimeout(() => {
+        resolve('Slow Promise');
+    }, 2000 );
+});
+
+const mediumPromise = () => new Promise ( resolve => {
+    setTimeout(() => {
+        resolve('Medium Promise');
+    }, 1500 );
+});
+
+const fastPromise = () => new Promise ( resolve => {
+    setTimeout(() => {
+        resolve('Fast Promise');
+    }, 1000 );
+});
---
+++ src/concepts/08-for-await.js
@@ -0,0 +1,53 @@
+import { heroes } from '../data/heroes';
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const forAwaitComponent = async( element ) => {
+
+    const id = '5d86371f2343e37870b91ef1'
+
+
+    // Se puede utilizar await en el argumento dentro de un IF
+    // if ( await getHeroAsync( id ) ) {
+    //     element.innerHTML = 'Sí existe ese héroe';
+    //     return;
+    // };
+
+    // element.innerHTML = 'No existe ese héroe';
+
+    const heroIds = heroes.map( hero => hero.id );
+
+    const heroPromises = getHeroesAsync( heroIds );
+
+    for await( const hero of heroPromises ) {
+        element.innerHTML += `${hero.name} <br/>`;
+    }
+
+}
+
+
+/**
+ * 
+ * @param {Array<String>} heroIds 
+ * @returns {Array<Promise>}
+ */
+const getHeroesAsync = ( heroIds ) => {
+    
+    const heroPromises = [];
+
+    heroIds.forEach( id => {
+        heroPromises.push( getHeroAsync(id)  );
+    });
+
+    return heroPromises;
+}
+
+const getHeroAsync = async(id) => {
+
+    await new Promise(( resolve ) => {
+        setTimeout(() => resolve(), 1000)
+    });
+
+    return heroes.find( hero => hero.id === id );
+}
